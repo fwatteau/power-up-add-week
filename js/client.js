709e83404505f5f4da0ecc2103eca71073cbb6fa
Kef <f.watteau@gmail.com>
2018-05-18 00:23:54 +0200

    V1 : Initial commit

--- js/client.js
+++ js/client.js
@@ -195,8 +195,9 @@ var boardButtonCallback = function(t){
 var cardButtonCallback = function(t, opts){
   const context = t.getContext();
   const card = t.card('id', 'due');
-  console.log(card);
+    console.log(card);
   card.due = new Date();
+    console.log(card);
   // Trello Power-Up Popups are actually pretty powerful
   // Searching is a pretty common use case, so why reinvent the wheel
   var items = ['acad', 'arch', 'badl', 'crla', 'grca', 'yell', 'yose'].map(function(parkCode){
