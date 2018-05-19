f6fcf5940f28e6f92e0e46ddcc589c9c8902cde0
Kef <f.watteau@gmail.com>
2018-05-19 23:55:50 +0200

    V1 : clean up

--- script/create-env.js
+++ script/create-env.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('./.env', `TRELLO_API_KEY=${build.environment.TRELLO_API_KEY}\n`);
+fs.writeFileSync('./.env', `TRELLO_API_KEY=${process.environment.TRELLO_API_KEY}\n`);
 console.log(fs.readFileSync('./.env'));
